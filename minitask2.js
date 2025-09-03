4578f33ee5e4cd078db10b41f217284678a14b4a
VirgilIw <imanuelambar11@gmail.com>
2025-09-03 17:37:35 +0700

    Add files via upload

--- minitask2.js
+++ minitask2.js
@@ -1,7 +1,28 @@
 // buatkan saya segitiga siku siku
 
-for (let i = 1; i < 7; i++) {
+// for (let i = 1; i < 7; i++) {
+//   let baris = "";
+//   for (let j = 1; j <= i; j++) {
+//     baris = baris + "*";
+//   }
+//   console.log(baris);
+// }
+
+// do while
+let i = 1;
+let tinggi = 5;
+
+do {
+  let j = 1;
   let baris = "";
-  for (let j = 1; j <= i; j++) baris = baris + "*";
+
+  do {
+    baris += "*";
+    j++;
+  } while (j <= i);
+
   console.log(baris);
-}
+  i++;
+} while (i <= tinggi);
+
+
